bfa0a7de5e279662ea700d73b21a7e2471609fcc
Dakota Solis <dakota.h.solis@gmail.com>
2021-03-29 20:25:51 -0500

    Add (mostly) empty project files

---
+++ RPS/rps_script.js
@@ -0,0 +1,17 @@
+/* Ask user if they want to play */
+let startGame = prompt("Would you like to play Rock, Paper, Scissors?")
+startGame.toLowerCase()
+/* if yes, play game */
+if (startGame == "yes" || "y"){
+console.log("You chose yes")
+}
+/* if no, exit */
+else {
+console.log("Okay bye den.")
+}
+/*start game */
+/*ask user for rock, paper, or scissors */
+/* generate rock, paper, or scissors from computer */
+/* tell user what both chose and print result */
+
+/* play game five rounds */
