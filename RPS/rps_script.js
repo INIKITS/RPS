44884821d16c91f969d955c6ba5c87ebad745ecb
Dakota Solis <dakota.h.solis@gmail.com>
2021-06-07 20:05:05 -0500

    Finished game of rock, paper, scissors.

--- RPS/rps_script.js
+++ RPS/rps_script.js
@@ -1,6 +1,102 @@
+
+
+//document.body.style.background = "url('https://i.imgur.com/sLVfSv9.jpg') no-repeat fixed center 30%";
+
+let numRounds = 0;
+let userWin = 0;
+let botWin = 0;
+let draw = 0;
+
+var buttonDiv = document.createElement('div');
+var scoreDiv = document.createElement('div');
+var matchResult = document.createElement('div');
+var win = document.createElement('p');
+var lose = document.createElement('p');
+
+//buttonDiv.setAttribute('id', 'userButtons')
+
+var rock = document.createElement("button");
+var paper = document.createElement("button");
+var scissors = document.createElement("button");
+
+win.setAttribute('id', 'win');
+lose.setAttribute('id', 'lose');
+
+rock.setAttribute('id', 'userButtons');
+paper.setAttribute('id', 'userButtons');
+scissors.setAttribute('id', 'userButtons');
+
+rock.setAttribute('class', 'rockButton');
+paper.setAttribute('class', 'paperButton');
+scissors.setAttribute('class', 'scissorButton');
+
+
+var userText = document.createElement('p');
+var botText = document.createElement('p');
+var drawText = document.createElement('p');
+
+userText.setAttribute('id', 'scoreboard');
+botText.setAttribute('id', 'scoreboard');
+drawText.setAttribute('id', 'scoreboard');
+
+
+userText.innerHTML = 'You: ';
+botText.innerHTML = 'Bot: ';
+drawText.innerHTML = 'Draw: ';
+
+matchResult.appendChild(win);
+matchResult.appendChild(lose);
+
+scoreDiv.appendChild(userText);
+scoreDiv.appendChild(botText);
+scoreDiv.appendChild(drawText);
+
+
+
+
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(scoreDiv);
+body.appendChild(buttonDiv);
+body.appendChild(matchResult);
+
+win.style.visibility='hidden';
+lose.style.visibility='hidden';
+
+win.innerHTML = 'You win!';
+lose.innerHTML = "You lose.";
+rock.innerHTML = "rock";
+paper.innerHTML = "paper";
+scissors.innerHTML = "scissors";
+
+buttonDiv.appendChild(rock);
+buttonDiv.appendChild(paper);
+buttonDiv.appendChild(scissors);
+
+
+
+
+rock.addEventListener("click", function(){
+    //userChoice('rock');
+    playGame('rock');
+    
+})
+paper.addEventListener("click", function(){
+    playGame('paper');
+    
+} )
+scissors.addEventListener ("click", function() {
+    playGame('scissors');
+    
+} )
+
+
+
+
+
 /* Ask user if they want to play */
 /* if yes, play game */
-while (true){
+/*while (true){
     let startGame = prompt("Would you like to play Rock, Paper, Scissors?")
     startGame = startGame.toLowerCase();
 
@@ -11,7 +107,7 @@ while (true){
         break;
     }
     /* if no, exit */
-    else if (startGame === "no"){
+    /*else if (startGame === "no"){
         console.log("Okay see ya later!")
         break;
     }
@@ -19,26 +115,28 @@ while (true){
         console.log("Try again")
         continue;
     }
-}
+} 
 
 /*start game */
 function playRound(userChoice, botChoice){
-    if (userChoice === botChoice){
-        return "Draw!";
+    
+        
+        if (userChoice === botChoice){
+            return draw++;
+        }
+        else if (userChoice === "rock" && botChoice === "scissors"){
+            return userWin++;
+        }
+        else if (userChoice === "paper" && botChoice === "rock"){
+            return userWin++;
+        }
+        else if (userChoice === "scissors" && botChoice === "paper"){
+            return userWin++;
+        }
+        else {
+            return botWin++;
+        }
     }
-    else if (userChoice === "rock" && botChoice === "scissors"){
-        return "You win!"
-    }
-    else if (userChoice === "paper" && botChoice === "rock"){
-        return "You win!"
-    }
-    else if (userChoice === "scissors" && botChoice === "paper"){
-        return "You win!"
-    }
-    else {
-        return "You lose!"
-    }
-}
 
 /* generate rock, paper, or scissors from computer */
 function computerChoice(){
@@ -58,10 +156,11 @@ function computerChoice(){
 
 }
 /*ask user for rock, paper, or scissors */
-function userChoice(){
-    while (true){
-        let choice = prompt("Rock, paper, or scissors?");
+function userChoice(choice){
+   /* while (true){
+        /*let choice = prompt("Rock, paper, or scissors?");
         choice = choice.toLowerCase();
+        */ 
 
         switch (choice){
             case 'rock':
@@ -72,26 +171,50 @@ function userChoice(){
                 return choice;
             default:
                 console.log("Invalid input, try again.")
-                continue;       
+                return null;       
         }
 
     }
-}
+
+function clearWin(){
+    win.style.visibility = 'hidden';
+    lose.style.visibility= "hidden";
+}    
+/*}
+
 
 
 /* tell user what both chose and print result */
-function playGame(){
+function playGame(userChoice){
     /* play game five rounds */
-    for (i=1; i<6; i++){
-        let round = i;
-        let user = userChoice();
-        let computer = computerChoice();
-        console.log("Round " + round + ":")
-        console.log("You chose " + user + " and the computer chose " + computer + ": ", 
-        playRound(user, computer) );
 
-        if (i == 4){
-            console.log("Thanks for playing!")
+        numRounds++;
+        let user = userChoice;
+        let computer = computerChoice();
+        console.log("Round " + numRounds + ":")
+        console.log("You chose " + user + " and the computer chose " + computer + ": ", 
+        playRound(user, computer));  
+        
+
+        if (userWin == 5){
+            win.style.visibility = 'visible';
+            setTimeout(clearWin, 1000);
+            numRounds = 0;
+            botWin = 0;
+            userWin = 0;
+            draw = 0;
         }
+        else if (botWin == 5){
+            lose.style.visibility = 'visible';
+            setTimeout(clearWin, 1000);
+            numRounds = 0;
+            botWin = 0;
+            userWin = 0;
+            draw = 0;
+
+        }
+        userText.innerHTML = 'You: ' + userWin;
+        botText.innerHTML = 'Bot: ' + botWin;
+        drawText.innerHTML = 'Draw: ' + draw;
     }
-}
+
